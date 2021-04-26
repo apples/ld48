d9c160ed4b8b99c46409b531ff7d6a60bc3baf5c
WiredOverload <mike.everywhere@gmail.com>
2021-04-26 00:27:53 -0500

    Added combat

--- client/player/Axe.gd
+++ client/player/Axe.gd
@@ -5,6 +5,10 @@ func swing():
 	show()
 	$CollisionShape2D.disabled = false
 	$AxeSprite.play()
+#	for body in get_overlapping_bodies():
+#		if(body.is_in_group("Enemy")):#body.name == "Enemy"):#change to group check
+#			print("hit: " + body.name)
+#			body.get_hit()
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,3 +23,9 @@ func _on_AxeSprite_animation_finished():
 	hide()
 	$CollisionShape2D.disabled = true
 	$AxeSprite.stop()
+
+
+func _on_Axe_body_entered(body):
+	if(body.is_in_group("Enemy")):#body.name == "Enemy"):#change to group check
+		print("hit: " + body.name)
+		body.get_hit()
