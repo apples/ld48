471e6f2cd573785bee87a6723ab12836dca69116
Apples <dbralir@gmail.com>
2021-04-30 22:01:54 -0500

    Added Silver font and fixed scaling

--- client/scenes/init/Init.gd
+++ client/scenes/init/Init.gd
@@ -2,10 +2,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$ConnectingPanel.hide()
 	$ErrorPanel.hide()
-	StrandService.connect("connected", self, "_on_StrandService_connected")
-	StrandService.connect("connect_failed", self, "_on_StrandService_connect_failed")
-	StrandService.login()
 
 func _on_StrandService_connected(id):
 	$AnimationPlayer.play("FadeOut")
@@ -26,6 +24,16 @@ func _on_RetryButton_pressed():
 	get_tree().reload_current_scene()
 
 func _on_OfflineButton_pressed():
+	$IntroPanel/LoginButton.disabled = true
+	$IntroPanel/OfflineButton.disabled = true
 	$ErrorPanel/RetryButton.disabled = true
 	$ErrorPanel/OfflineButton.disabled = true
 	$AnimationPlayer.play("FadeOut")
+
+
+func _on_LoginButton_pressed():
+	StrandService.connect("connected", self, "_on_StrandService_connected")
+	StrandService.connect("connect_failed", self, "_on_StrandService_connect_failed")
+	StrandService.login()
+	$ConnectingPanel.show()
+	$IntroPanel.hide()
